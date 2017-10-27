7860aaee4ebcb677c5d3fed3e53b6f72354faa3e
mingyangzhang <zmyrzwd@gmail.com>
2017-10-27 20:23:38 +0800

    log for packet in

--- ofproto/ofproto-dpif-xlate.c
+++ ofproto/ofproto-dpif-xlate.c
@@ -4520,11 +4520,23 @@ execute_controller_action(struct xlate_ctx *ctx, int len,
         && ctx->rule && rule_is_table_miss(&ctx->rule->up)) {
         reason = OFPR_EXPLICIT_MISS;
     }
-
-    size_t packet_len = dp_packet_size(packet);
-	if(reason == OFPR_EXPLICIT_MISS){
-		printf("\n\n\n\n upcall missed! \n\n\n\n");
+	/* test for packet in */
+	if(reason == OFPR_NO_MATCH){
+		printf("\n\n\n OFPR_NO_MATCH \n\n\n");
 	}
+	if(reason == OFPR_ACTION){
+		printf("\n\n\n OFPR_ACTION \n\n\n");
+	}
+	if(reason == OFPR_EXPLICIT_MISS){
+		printf("\n\n\n OFPR_EXPLICIT_MISS \n\n\n");
+	}
+	if(reason == OFPR_IMPLICIT_MISS){
+		printf("\n\n\n OFPR_IMPLICIT_MISS \n\n\n");
+	}
+	if(reason == OFPR_N_REASONS){
+		printf("\n\n\n OFPR_N_REASONS \n\n\n");
+	}
+    size_t packet_len = dp_packet_size(packet);
     struct ofproto_async_msg *am = xmalloc(sizeof *am);
     *am = (struct ofproto_async_msg) {
         .controller_id = controller_id,
